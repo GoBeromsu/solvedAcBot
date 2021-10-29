87a5e933e5101eb89e6faddde429c2661c167669
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2021-10-29 23:46:17 +0900

    add CRUD function for issue

---
+++ issue.py
@@ -0,0 +1,17 @@
+from github import Github
+import config
+import solved
+
+g = Github(config.GITHUB_ACCESS_KEY)
+
+def getIssue(repo_url,issue_num):
+    issue = g.get_repo(repo_url).get_issue(number=issue_num)
+    return issue.body
+
+def updateIssue(repo_url,issue_num,content):
+    issue = g.get_repo(repo_url).get_issue(number=issue_num)
+    issue.edit(body=content)
+
+def createIssue(repo_url,content):
+    issue = g.get_repo(repo_url).create_issue(title='Solved Problems',body=content)
+
