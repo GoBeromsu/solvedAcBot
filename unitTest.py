241891adf50e2e862d15960e2c213d6c7e4f9231
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2021-10-31 22:35:30 +0900

    add test code for problemSetting

--- unitTest.py
+++ unitTest.py
@@ -1,12 +1,18 @@
 import requests
 import unittest
-
+import config
 import solved
-
+import github
 class MyTest(unittest.TestCase):
-    def test_urlSettings(self):
-        request = requests.get(solved.UrlSettings("310O").userSolvedUrl)
+    def test_problemSetting(self):
+        request = requests.get(solved.ProblemSettings().userSolvedUrl)
         self.assertEqual(request.status_code, 200)
-
+    def test_getSolved(self):
+        self.assertMultiLineEqual(f"{type(solved.ProblemSettings().getSolved())}","<class 'list'>")
+    def test_updateProblems(self):
+        return True
+    def test_checkProblems(self):
+        return True
+    
 if __name__=="__main__":
     unittest.main()
