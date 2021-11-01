001ff9b7f1f911ed88f6e5c9de4ca097f48b77f2
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2021-11-01 12:14:54 +0900

    add getSolvedToday(), changeProblemsToStr(), updateProblems()

--- run.py
+++ run.py
@@ -1,38 +1,51 @@
 import issue
+
 import config
 import solved
-# 오늘까지 푼 문제와 어제까지 푼 문제를 비교
-# 비교 후 같으면 트윗 X, 다르면 트윗
-# 두 입력 값의 길이를 비교함
-# 길이가 다른 경우 첫 인덱스부터 비교해가며 다른 값이 나올 경우 스택에 넣고
-# 다시 그 자리에서 다시 시작함
 
-def updateProblems():
-    pass
-
-def checkProblems(bProblems:list,nProblems:list):
-    return True if len(bProblems) == len(nProblems) else False
+def getSolvedToday(bSolved,nSolved):
+    stack = []
+    for problem in nSolved:
+        if problem not in bSolved:
+            stack.append(problem)
+    return stack
+def changeProblemsToList(body:str):
     
-def formatProblems(body:str):
     problems = []
     for problem in body.split('\n'):
-        idx = problem.find(' ')
-        number = problem[:idx]
-        title = problem[idx+1::]
-        problems.append([number,title])
-    problems.pop()
+        if problem != '':                
+            idx = problem.find(' ')
+            number = int(problem[:idx])
+            title = problem[idx+1::]
+            problems.append([number,title])
+
     return problems
-    
+def changeProblemsToStr(problems:list):
+    body =""
+    for problem in problems:
+        number = problem[0]
+        title = problem[1]
+        body+=f"{number} {title}\n"
+    return body
+def updateProblems(nowSolved):
+    issue.updateIssue(config.GITHUB_REPO_URL,1,changeProblemsToStr(nowSolved))
+def checkProblemsChanged(beforeSolved:list,nowSolved:list):
+    return True if len(beforeSolved) != len(nowSolved) else False 
+
 
 def main():
-    beforeSolvedProblems = formatProblems(issue.getIssue(config.GITHUB_REPO_URL, 1))
-    nowSolvedProblems = solved.getSolved()
-    print(len(beforeSolvedProblems))
-    print(len(nowSolvedProblems))
-    if not checkProblems(beforeSolvedProblems, nowSolvedProblems):
-        pass
+
+    beforeSolved = changeProblemsToList(issue.getIssue(config.GITHUB_REPO_URL, 1))
+    nowSolved = solved.ProblemSettings().getSolved()
+
+    if checkProblemsChanged(beforeSolved, nowSolved):
+        solvedToday = getSolvedToday(beforeSolved, nowSolved)
+        updateProblems(nowSolved)
+        print(solvedToday)
     else:
-        pass
+        print("오늘 안 풀었구먼")
+
+
 
 if __name__=="__main__":
     main()
