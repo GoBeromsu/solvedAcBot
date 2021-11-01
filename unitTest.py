8c0b71fc14085853a829591904147bc28ed331b9
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2021-11-01 12:49:13 +0900

    add testCode for getSolved() && getSolvedToday() && checkProblemsChanged()

--- unitTest.py
+++ unitTest.py
@@ -3,16 +3,24 @@ import unittest
 import config
 import solved
 import github
+import issue
+import run
 class MyTest(unittest.TestCase):
+    def setUp(self):
+        self.beforeSolved = run.changeProblemsToList(issue.getIssue(config.GITHUB_REPO_URL, 1))
+        self.nowSolved = solved.ProblemSettings().getSolved()
+
     def test_problemSetting(self):
         request = requests.get(solved.ProblemSettings().userSolvedUrl)
         self.assertEqual(request.status_code, 200)
     def test_getSolved(self):
         self.assertMultiLineEqual(f"{type(solved.ProblemSettings().getSolved())}","<class 'list'>")
-    def test_updateProblems(self):
-        return True
-    def test_checkProblems(self):
-        return True
-    
+    def test_getSolvedToday(self):
+        if run.getSolvedToday(self.beforeSolved,self.nowSolved):
+            return True
+        else:
+            return False
+    def test_checkProblemsChanged(self):
+        self.assertEqual(bool,type(run.checkProblemsChanged(self.beforeSolved,self.nowSolved)))
 if __name__=="__main__":
     unittest.main()
