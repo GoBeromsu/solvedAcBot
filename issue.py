cdc6a824f8fd4e20b3dd641c8fb523725a6b2175
GoBeromsu <37897508+GoBeromsu@users.noreply.github.com>
2021-11-09 12:33:08 +0900

    test issue.py

--- issue.py
+++ issue.py
@@ -4,6 +4,8 @@ import solved
 
 g = Github(config.GITHUB_ACCESS_KEY)
 print(g)
+print(g.get_repo(config.GITHUB_REPO_URL))
+print(g.get_repo(config.GITHUB_REPO_URL).get_issue(number=1))
 def getIssue(repo_url,issue_num):
     issue = g.get_repo(repo_url).get_issue(number=issue_num)
     return issue.body
